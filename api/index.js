c8099500b514f557a903620ffcc92c747513eea7
Devin Sharpe <devinrsharpe@gmail.com>
2022-05-21 15:48:30 -0400

    feat: add log test route

--- api/index.js
+++ api/index.js
@@ -21,6 +21,11 @@ app.get("/api/item/:slug", function (req, reply) {
   reply.code(200).send(`Item ${slug}`);
 });
 
+app.get("/api/test", function (req, reply) {
+  console.log("hello logs");
+  reply.code(200).send("hello logs");
+});
+
 export default async (req, res) => {
   await app.ready();
   app.server.emit("request", req, res);
